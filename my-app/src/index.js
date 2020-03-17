324716ee17d97eea0d2b0304127985d97959cd49
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-03-17 17:24:17 +0900

    JSX notation

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+//JSX 記法
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    element,
+    // index.html の root
     document.getElementById('root')
 );
