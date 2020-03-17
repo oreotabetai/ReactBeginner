e02332e0055fc56674a758f48902e6c953c83f6b
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-03-17 18:22:33 +0900

    large component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,41 @@ import ReactDOM from 'react-dom';
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
+// author text date
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar"
+                    src={props.author.avatarUrl}
+                    alt={props.author.name}
+                />
+                <div className="UserInfo-namee">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
 
-const element = <Welcome name="sara" />;
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    element,
+    <App />,
     document.getElementById('root')
 );
